8251c652108511f4def75a50fb5fb34d34da7f6a
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-10-09 20:54:44 -0500

    feat:initial_app_proyecto_1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
+//import { HomeComponent } from '@components/home/home.component';
 
 
 export const routes: Routes = [
-
+  // {
+  //   path: '',
+  //   component: HomeComponent
+  // }
+  {
+    path: '',
+    loadChildren: () => import('./pages/private/private.routes').then(m => m.privateRoute)
+  }
 ];
---
+++ src/app/pages/private/private.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { PrivateComponent } from './private.component';
+
+export const privateRoute: Routes = [
+  {
+    path: "",
+    component:PrivateComponent,
+  }
+];
+
+
